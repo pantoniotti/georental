3b3605c4f63aa914fa9705b4489cb452f141f922
pantoniotti <pantoniotti@gmail.com>
2017-01-30 21:18:19 -0500

    Added css and fixed specs

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,5 +1,5 @@
 class Rental < ActiveRecord::Base
-  has_many :travels, inverse_of: :rental, dependent: :destroy
+  has_many :travels, inverse_of: :rental, foreign_key: :rental_id
 
   enum make: %w(Honda Kia Nissan Hyundai Wolkswagen Ford Peugeot Bmw Dodge)
   enum model: %w(CRV Civic Prius Charger Grand-caravan 106 320i)
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -6,24 +6,19 @@ RSpec.describe Rental, type: :model do
     Rental.new(make: 0, model: 0, vehicle: 0, engine: 1, energy: 1)
   end
 
-  let(:travel) do
-    Travel.new(rental_id: 1, start_time: 2.days.ago, end_time: DateTime.now, distance: 12.45)
-  end
-
-  let(:rental_missing_params) do
-    Rental.new(make: 0, model: 0)
-  end
-
-  it 'is not valid when some params are missing' do
-    expect(rental_missing_params.valid?).to be false
-  end
-
   it 'is valid when all params are present' do
     expect(rental.valid?).to be true
   end
 
+  it 'is not valid when some params are missing' do
+    rental.make = nil
+    expect(rental.valid?).to be false
+  end
+
   it 'has travels' do
-    expect(rental.travels).to eq 1
+    rental.travels.build(rental_id: 1, start_time: 2.days.ago, end_time: DateTime.now, distance: 12.45)
+    rental.save!
+    expect(rental.travels.count).to eq 1
   end
 
 
